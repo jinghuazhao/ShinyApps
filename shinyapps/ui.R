5de59dd3cd4d6d334f17afcedc6c7e31cf81fe06
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-05-19 16:33:56 +0100

    shinyapps

--- shinyapps/ui.R
+++ shinyapps/ui.R
@@ -18,11 +18,7 @@ shinyUI(fluidPage(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30)
         ),
 
         # Show a plot of the generated distribution
