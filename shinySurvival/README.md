5fa9d4c86a5c87f4cc9a95ecf0bb86565e72bf40
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-05-19 21:29:37 +0100

    shinySurvival

--- shinySurvival/README.md
+++ shinySurvival/README.md
@@ -1,9 +1,19 @@
-This is a very early attempt to seed Shiny projects via the familiar survival analysis. 
-It envisages a scenario that an end-user can upload his/her own data for a survival analysis and download the analysis report. The following items are experimented:
+This is a very early attempt to seed Shiny projects via the familiar survival analysis, 
+
+whose source code is available from [https://github.com/jinghuazhao/ShinyApps/](https://github.com/jinghuazhao/ShinyApps/)
+
+An end-user can upload his/her own data for a survival analysis and download the analysis report. The following aspects are experimented with data 
+from R/survival package:
+
+```r
+library(survival)
+head(lung)
+write.csv(survival::lung,file="lung.csv",quote=FALSE,row.names=FALSE)
+```
 
 ### Upload data
 
-This is a local file in .csv or .tsv format.
+This is a local file in .csv or .tsv format such as `lung.tsv` from above.
 
 ### Kaplan-Meier curve
 
@@ -16,5 +26,3 @@ This is an option to download a file in .tsv format.
 ### Generate report
 
 This is a PDF file containing the commands and plots.
-
-The source code of this is available from https://github.com/jinghuazhao/ShinyApps/.
--- shinySurvival/ui.R
+++ shinySurvival/ui.R
@@ -2,14 +2,15 @@ source("init.R")
 
 ui <- dashboardPage(
   title = "shinySurvival",
-  dashboardHeader(title = tags$a(href='https://github.com/jinghuazhao/ShinyApps', target = '_blank',
-                                 tags$img(src=paste0("bees.svg"), height = "70%", width = "auto", align = "middle")),
-                  dropdownMenu(type = "messages",
-                               tags$li(HTML('<li><a href="https://github.com/jinghuazhao/ShinyApps" target="_blank"><i class="fa fa-code-branch"></i><h4>GitHub</h4></a></li>')),
-                               tags$li(HTML('<li><a href="mailto:jinghuazhao@hotmail.com" target="_blank"><i class="fa fa-question"></i><h4>email</h4></a></li>'))
-
-                  )
-                 ),
+  dashboardHeader(
+     title = tags$a(href='https://github.com/jinghuazhao/ShinyApps', target = '_blank',
+                    tags$img(src=paste0("bees.svg"), height = "70%", width = "auto", align = "middle")
+     ),
+     dropdownMenu(type = "messages",
+        tags$li(HTML('<li><a href="https://github.com/jinghuazhao/ShinyApps" target="_blank"><i class="fa fa-code-branch"></i><h4>GitHub</h4></a></li>')),
+        tags$li(HTML('<li><a href="mailto:jinghuazhao@hotmail.com" target="_blank"><i class="fa fa-question"></i><h4>email</h4></a></li>'))
+     )
+  ),
   dashboardSidebar(
     sidebarMenu(
       menuItem("Home", tabName = "landing", icon = icon("home")),
@@ -22,10 +23,16 @@ ui <- dashboardPage(
   dashboardBody(
     tabItems(
       tabItem(tabName = "landing",
-              div(class = "jumbotron", HTML("<center><h1>shinySurvival - a survival analysis taster</h1></center>")),
-              fluidRow(
-                fluidRow(div(class = "col-sm-12", div(class = "box box-primary", style = "padding-right: 5%; padding-left: 5%; font-size:110%", NULL, div(class = "box-body", shiny::includeMarkdown("README.md")))
-              )))),
+         div(class = "jumbotron", HTML("<center><h1>Survival analysis using Shiny</h1></center>")),
+         fluidRow(
+            fluidRow(div(class = "col-sm-12", 
+                         div(class = "box box-primary", style = "padding-right: 5%; padding-left: 5%; font-size:100%", NULL,
+                             div(class = "box-body", shiny::includeMarkdown("README.md"))
+                         )
+                     )
+            )
+         )
+      ),
       tabItem(tabName = "Upload-data",
         h2("Upload a csv or tsv file"),
         fluidRow(
@@ -46,6 +53,6 @@ ui <- dashboardPage(
         h2("Generate analysis report"),
         downloadButton("report", "Generate report")
       )
-     )
+    )
   )
 )
